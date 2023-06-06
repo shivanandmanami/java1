4988dee7e212d929ed2f9f1a86fd75674e260128
shivanandmanami <131964804+shivanandmanami@users.noreply.github.com>
2023-06-06 11:32:19 +0530

    Add files via upload

---
+++ fibonacciSeries.py
@@ -0,0 +1,12 @@
+num =int(input("enter the number:"))
+n1,n2= 0,1
+sum=0
+
+if num<=0:
+    print("plzz enter the number greater than 0:")
+else:
+    for i in range(0,num):
+        print(sum, end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
